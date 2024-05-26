d939e5da9f3b1622825a04b1cd744ab183cb9bf7
Jose.Xavier <lucasxaviert875@gmail.com>
2024-05-26 20:15:24 -0300

    Refactor update_effect_y

--- kivy/uix/scrollview.py
+++ kivy/uix/scrollview.py
@@ -651,6 +651,27 @@ class ScrollView(StencilView):
             self.scroll_y = -sy
         self._trigger_update_from_scroll()
 
+
+    def _update_effect_y(self, *args):
+        vp = self._viewport
+        if not vp or not self.effect_y:
+            return
+        if self.effect_y.is_manual:
+            sh = vp.height - self._effect_y_start_height
+        else:
+            sh = vp.height - self.height
+        if sh < 1 and not (self.always_overscroll and self.do_scroll_y):
+            return
+        if sh != 0:
+            sy = self.effect_y.scroll / sh
+            self.scroll_y = -sy
+        else:
+            self.scroll_y = 1 - self.effect_y.scroll
+        self._trigger_update_from_scroll()
+
+
+
+
     def to_local(self, x, y, **k):
         tx, ty = self.g_translate.xy
         return x - tx, y - ty
